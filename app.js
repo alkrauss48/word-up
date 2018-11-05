c578f55e08f8f5082e4fba4af4838d7f5920f7cd
aaron <alkrauss48@gmail.com>
2018-11-05 12:20:54 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready( function() {
+  var fontSize = 160;
+
+  setInterval( function() {
+    // Height of the window.
+    var windowHeight = window.innerHeight;
+    var scrollHeight = $('#wordup').prop('scrollHeight');
+
+    console.log(windowHeight);
+    console.log(scrollHeight);
+
+    // Boolean vars to see if the text is too big or too small.
+    var textTooBig = scrollHeight >= windowHeight - 30;
+    var textTooSmall = scrollHeight <= windowHeight - 50;
+
+    // If the text is too big, shrink it.
+    if (textTooBig) {
+      console.log('too big');
+      fontSize -= 5;
+      $('#wordup').css('font-size', fontSize);
+
+      var newScrollHeight = $('#wordup').prop('scrollHeight');
+      $('#wordup').height(0).height(newScrollHeight);
+    }
+
+    // If the text is too small, increase it.
+    if (textTooSmall) {
+      console.log('too small');
+      fontSize += 5;
+      $('#wordup').css('font-size', fontSize);
+
+      var newScrollHeight = $('#wordup').prop('scrollHeight');
+      $('#wordup').height(0).height(newScrollHeight);
+    }
+  }.bind(this), 100);
+});
