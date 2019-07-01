214bfe2a5ee27eb2805481b74d000664dfbbf120
silashuereca <silasgranthuereca@gmail.com>
2019-06-30 23:30:34 -0500

    Changing layout

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-$(document).ready( function() {
-  var fontSize = 160;
+$(document).ready(function() {
+  $("#open").click(function() {
+    $("#overlay").css("left", "0px");
+    console.log("hello");
+  });
 
-  setInterval( function() {
-    // Height of the window and scroll area.
-    var windowHeight = window.innerHeight;
-    var scrollHeight = $('#wordup').prop('scrollHeight');
+  $("#close").click(function() {
+    $("#overlay").css("left", "-400px");
+    console.log("hello");
+  });
 
-    console.log(windowHeight);
-    console.log(scrollHeight);
-
-    // Boolean vars to see if the text is too big or too small.
-    var textTooBig = scrollHeight >= windowHeight - 30;
-    var textTooSmall = scrollHeight <= windowHeight - 50;
-
-    // If the text is too big, shrink it.
-    if (textTooBig) {
-      console.log('too big');
-      fontSize -= 5;
-      $('#wordup').css('font-size', fontSize);
-
-      var newScrollHeight = $('#wordup').prop('scrollHeight');
-      $('#wordup').height(0).height(newScrollHeight);
-    }
-
-    // If the text is too small, increase it.
-    if (textTooSmall) {
-      console.log('too small');
-      fontSize += 5;
-      $('#wordup').css('font-size', fontSize);
-
-      var newScrollHeight = $('#wordup').prop('scrollHeight');
-      $('#wordup').height(0).height(newScrollHeight);
-    }
-  }, 100);
+  // var fontSize = 160;
+  // setInterval( function() {
+  //   // Height of the window and scroll area.
+  //   var windowHeight = window.innerHeight;
+  //   var scrollHeight = $('#wordup').prop('scrollHeight');
+  //   console.log(windowHeight);
+  //   console.log(scrollHeight);
+  //   // Boolean vars to see if the text is too big or too small.
+  //   var textTooBig = scrollHeight >= windowHeight - 30;
+  //   var textTooSmall = scrollHeight <= windowHeight - 50;
+  //   // If the text is too big, shrink it.
+  //   if (textTooBig) {
+  //     console.log('too big');
+  //     fontSize -= 5;
+  //     $('#wordup').css('font-size', fontSize);
+  //     var newScrollHeight = $('#wordup').prop('scrollHeight');
+  //     $('#wordup').height(0).height(newScrollHeight);
+  //   }
+  //   // If the text is too small, increase it.
+  //   if (textTooSmall) {
+  //     console.log('too small');
+  //     fontSize += 5;
+  //     $('#wordup').css('font-size', fontSize);
+  //     var newScrollHeight = $('#wordup').prop('scrollHeight');
+  //     $('#wordup').height(0).height(newScrollHeight);
+  //   }
+  // }, 100);
 });
