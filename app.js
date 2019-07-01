8435f6b3cb43505d5d02c9aee623aeddf6a28328
silashuereca <silasgranthuereca@gmail.com>
2019-07-01 01:22:57 -0500

    Finished display message web application

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
+  // Opening overlay
   $("#open").click(function() {
     $("#overlay").css("left", "0px");
-    console.log("hello");
   });
-
+  // Closing overlay
   $("#close").click(function() {
     $("#overlay").css("left", "-400px");
-    console.log("hello");
+  });
+
+  // Submitting text input value to the h1 container
+  $("#form").submit(function(e) {
+    e.preventDefault();
+    let inputValue = $("#wordup").val();
+
+    //closes the overlay bar with enter submit
+    $("#overlay").css("left", "-400px");
+
+    // appends text and makes text responsive in h1 container
+    $("span").text(inputValue);
+    $("h1").textfill({
+      maxFontPixels: 1000
+    });
   });
 
   // var fontSize = 160;
